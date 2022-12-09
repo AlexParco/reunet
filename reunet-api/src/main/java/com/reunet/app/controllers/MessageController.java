429c42b86062326177edf1917cd954df7e8e96e0
Alexander Parco <alexparco16@gmail.com>
2022-12-09 14:43:50 -0500

    feat(api): add delete message controller

--- reunet-api/src/main/java/com/reunet/app/controllers/MessageController.java
+++ reunet-api/src/main/java/com/reunet/app/controllers/MessageController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -67,6 +68,26 @@ public class MessageController {
                     HttpServletResponse.SC_OK,
                     "",
                     messages));
+
+        } catch (Exception e) {
+
+            return ResponseEntity.internalServerError().body(new Response<List<Message>>(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    e.getMessage(),
+                    null));
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Response<List<Message>>> deleteMensajeById(@RequestParam Long id) {
+        try {
+
+            messageServices.delete(id);
+
+            return ResponseEntity.ok().body(new Response<List<Message>>(
+                    HttpServletResponse.SC_OK,
+                    "",
+                    null));
 
         } catch (Exception e) {
 
